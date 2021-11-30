60850e8b4f42ddcd0e914d5c2fb406073f1e12fa
mimarciniak <marciniak.michal91@gmail.com>
2021-11-30 20:58:01 +0100

    method modification

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -3,8 +3,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions (
-        features = {"src/test/resources"}
-)
+@CucumberOptions (plugin = {"pretty"})
+
 public class CucumberRunner {
 }
--- src/test/java/StepsDef.java
+++ src/test/java/StepsDef.java
@@ -1,23 +1,25 @@
+import Calculator.Calc;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 
 public class StepsDef {
+    Calc calc;
+    int result;
 
     @Given("I test calculator")
     public void i_test_calculator() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        calc = new Calc();
     }
 
     @When("I add {int} and {int}")
     public void i_add_and(Integer int1, Integer int2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        result = calc.sum(int1, int2);
     }
+
     @Then("result is {int}")
     public void result_is(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Assertions.assertEquals(int1, result);
     }
 }
